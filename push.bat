15cdca1304e83c42bf8a3157de96abb3ace1fbe0
fyto02 <fabriziogrela8@gmail.com>
2026-05-09 15:38:00 -0300

    DevSecOps Toolkit v2.0 optimizado

--- push.bat
+++ push.bat
@@ -1,12 +1,15 @@
 @echo off
 echo =====================================
-echo   Subir cambios a GitHub (Push)
+echo   Subir cambios a GitHub (Rama MAIN)
 echo =====================================
 echo.
 set /p mensaje="Ingresa el mensaje para este cambio: "
 git add .
 git commit -m "%mensaje%"
-git push
+git checkout main
+git merge v2-development
+git push origin main
+git checkout v2-development
 echo.
-echo [OK] ¡Cambios subidos con exito!
+echo [OK] ¡Cambios fusionados y subidos a MAIN con exito!
 pause
